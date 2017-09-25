038770f8d03038cbb3090770eb85f4f2fbf26b53
Russell Schwartz <russell.w.schwartz@gmail.com>
2017-09-25 17:05:00 -0400

    Program flow changes

--- Evolution Simulations/Walking.py
+++ Evolution Simulations/Walking.py
@@ -79,7 +79,10 @@ def get_walking_distance(organism, time_limit):
     return 0
 
 
-def watch(screen, organism):
+def watch(organism, screen=None):
+    if not screen:
+        screen = pg.display.set_mode((400, 500))
+
     organism.reset_to_start()
     avg = sum([node.pos.x for node in organism.nodes]) / organism.num_nodes
     for node in organism.nodes:
@@ -115,21 +118,25 @@ def watch(screen, organism):
                     fps += 1
                 elif event.button == 5:
                     fps = max(1, fps - 1)
+
+        avg_pos =  sum([node.pos.x for node in organism.nodes]) / organism.num_nodes
+        if avg_pos >= screen.get_width() - 300:
+            cam_x = int(avg_pos) - 100
+
         cam_x += cam_x_vel
         screen.fill((255, 255, 255))
         for i in range(-1 + cam_x // 100, screen.get_width() // 100 + 1 + cam_x // 100):
             pg.draw.line(screen, (0, 0, 0), (i * 100 - cam_x, 0), (i * 100 - cam_x, screen.get_height()), 2)
         organism.draw_on(screen, (100 - cam_x, 0))
         pg.display.update()
+
         organism.apply_physics()
         tick += 1
-    screen.fill((255, 255, 255))
-    pg.display.update()
+
+    pg.display.quit()
 
 
 if __name__ == "__main__":
-    screen = pg.display.set_mode((1000, 500))
-
     population_size = 500
     starting_organisms = [Organism() for _ in range(population_size)]
     population = Generation(starting_organisms)
@@ -139,7 +146,7 @@ if __name__ == "__main__":
         population.sort()
         best, median, worst = population.get_best(), population.get_median(), population.get_worst()
         species_distribution = population.get_species_distribution()
-        print("Generation {0}: best = {1}    median = {2}    worst = {3}    species = {4}".format(i, round(best.fitness, 3), round(median.fitness, 3), round(worst.fitness, 3) , species_distribution))
+        print("Generation {0}: best = {1}    median = {2}    worst = {3}    species = {4}".format(i, round(best.fitness, 3), round(median.fitness, 3), round(worst.fitness, 3), species_distribution))
         # display(screen, [best, median, worst])
-        watch(screen, best)
+        watch(best)
         population = population.get_next_generation()
